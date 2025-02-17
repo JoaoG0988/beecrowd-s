72378b82495a96e6ea7fdf96253c5963d7cb20a1
João Gabriel MF <232003607@aluno.unb.br>
2025-02-17 13:55:03 -0300

    1244.py modified

--- python/1244.py
+++ python/1244.py
@@ -3,20 +3,7 @@ num = int(input())
 
 for i in range(num):
     string = input().split()
-    n = len(string)
-    for i in range(n):
-        for j in range(0,n-i-1):
-            if len(string[j]) < len(string[j+1]):
-                string[j],string[j+1] = string[j+1],string[j]
-        
+    string.sort(key = len, reverse = True)
+    
     print(' '.join(string))
-        
-
-     
-
-            
-        
-        
-        
-        
         
